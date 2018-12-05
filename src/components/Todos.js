6604436a0b97cd96fc5d88ca6da23fe6f06eb7ef
KYLE\Surface <kyle.public10415@gmail.com>
2018-12-06 00:24:40 +0800

    feat : add function to Todo.js for handling todo item onclick

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Todos extends Component {
+  changeStatus = (id)=>{
+    this.props.changeStatus(id)
+  }
+  
   render() {
     const {todos} = this.props
     return (
       <div>
-        {todos.map((todo) => <li key={todo.id}>{todo.content}</li>)}
+        {todos.map((todo) => <li key={todo.id} onClick={this.changeStatus(todo.id)}>{todo.content}</li>)}
       </div>
     )
   }
--- src/containers/TodoInputContainer.js
+++ src/containers/TodoInputContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+
 import { connect } from 'react-redux'
 import TodoInput from '../components/TodoInput.js'
 
